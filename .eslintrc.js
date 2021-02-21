3dada504f957dd2d37581569c8821fda69f41c1b
Jian Feng Ye <yejianfengblue@163.com>
2021-02-21 11:22:54 +0800

    Set vue/max-len to 120

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/max-len": ["warn", { code: 120, comments: 120, template: 120 }],
   },
   overrides: [
     {
