ef091666e43c9bb5e8d11d4ab5670d1c341ee69a
Jian Feng Ye <yejianfengblue@163.com>
2021-02-22 23:03:44 +0800

    Navigation drawer adds app sub group

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -8,6 +8,23 @@
         <v-list-item-title v-text="link.title" />
       </v-list-item>
     </v-list>
+
+    <v-list>
+      <v-list-group v-for="appLink in appLinks" :key="appLink.title" no-action>
+        <template v-slot:activator>
+          <v-list-item-avatar>
+            <v-img :src="appLink.avatar" />
+          </v-list-item-avatar>
+          <v-list-item-title v-text="appLink.title" />
+        </template>
+        <v-list-item v-for="subItem in appLink.subItems" :key="subItem.title" router :to="subItem.route" dense>
+          <v-list-item-icon>
+            <v-icon v-text="subItem.icon" />
+          </v-list-item-icon>
+          <v-list-item-title>{{ subItem.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
   </v-navigation-drawer>
 </template>
 
@@ -23,9 +40,27 @@ export default class Drawer extends Vue {
     { icon: "mdi-clock-outline", title: "Schedules", route: "/schedules" },
     { icon: "mdi-clipboard-check-outline", title: "Tasks", route: "/tasks" },
   ];
+
+  private appLinks: Array<AppLink> = [
+    {
+      avatar: require("@/assets/duel-links-mdpi.png"),
+      title: "Duel Links",
+      subItems: [
+        { icon: "mdi-account", title: "Accounts", route: "/duel-links-accounts" },
+        { icon: "mdi-diamond-stone", title: "Gem", route: "/duel-links-gem" },
+        { icon: "mdi-human-handsup", title: "Character", route: "/duel-links-character" },
+        { icon: "mdi-bullseye-arrow", title: "Stage", route: "/duel-links-stage" },
+      ],
+    },
+  ];
 }
 
 class NavLink {
   constructor(readonly icon: string, readonly title: string, readonly route: string) {}
 }
+
+class AppLink {
+  constructor(readonly avatar: string, readonly title: string, readonly subItems: Array<NavLink>) {}
+}
+
 </script>
